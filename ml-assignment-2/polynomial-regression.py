90bb7d6cc33f9dd0cb874bcf291351756b49a091
Amna <ammy5342@gmail.com>
2024-03-20 23:20:35 +0200

    Exported notebook as Python script

--- ml-assignment-2/polynomial-regression.py
+++ ml-assignment-2/polynomial-regression.py
@@ -1,68 +1,97 @@
+
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
-from sklearn import metrics
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import PolynomialFeatures
 
-#Load players data
+
+
+MODEL_DEGREE=2
+
+
 data = pd.read_csv('assignment2dataset.csv')
-#Drop the rows that contain missing values
-data.dropna(how='any',inplace=True)
-print(data.shape)
-X=data.iloc[:,1:6] #Features
+data.info()
+
+
+
+X=data.drop('Performance Index', axis=1,)#Features
 Y=data['Performance Index'] #Label
-
-student_data = data.iloc[:,:]
-
-le=LabelEncoder()
-le.fit(student_data['Extracurricular Activities'])
-student_data['Extracurricular Activities']= le.transform(student_data['Extracurricular Activities'])
-#Feature Selection
-#Get the correlation between the features
-# corr = student_data.corr()
-# #Top 50% Correlation training features with the Value
-# top_feature = corr.index[abs(corr['Value'])>0.5]
-# #Correlation plot
-# top_corr = student_data[top_feature].corr()
-# top_feature = top_feature.delete(-1)
-# X = X[top_feature]
-
 #Split the data to training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.30,shuffle=True,random_state=10)
 
-#X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size = 0.50,shuffle=True)
+
+
+le=LabelEncoder()
+X_train['Extracurricular Activities'] = le.fit_transform(X_train['Extracurricular Activities'])
+X_train.info()
+
+
+
+train_data = pd.concat([X_train, y_train], axis=1)
+
+
+#Get the correlation between the features
+corr = train_data.corr()
+#Top 50% Correlation training features with the Value
+top_feature = corr.index[abs(corr['Performance Index'])>0.25]
+#Correlation plot
+top_feature = top_feature.delete(-1)
+X_train = X_train[top_feature]
+X.info()
+
+
+
+
 
 def PolynomialFeatures(X, degree):
-    numOfRows =X.shape[0] 
-    numOfCols= X.shape[1]
-    for i in range(degree):
-        for feature1 in range(numOfCols):
-            for feature2 in range(numOfCols):
-                newFeature = X.columns[feature1].strip()+"*"+X.columns[feature2].strip()
-                X.insert(X.shape[1],newFeature,0)
-                X[newFeature]=X.iloc[:,feature1]*X.iloc[:,feature2]
-# transforms the existing features to higher degree features.
-X_train_poly = PolynomialFeatures(X_train, degree=2)
+    # transforms the existing features to higher degree features.
+    originalFeatures =list(X.columns)
+
+    for i in range(degree-1):
+        columns =list(X.columns)
+        for feature1 in list(columns):
+            for feature2 in list(originalFeatures):
+                newFeature = feature1 +"*"+feature2
+                newFeatureReversed = feature2 + "*" + feature1
+                if newFeature not in X and newFeatureReversed not in X:
+                    #X.insert(X.shape[1],newFeature,0)
+                    newColumn=X.loc[:,feature1]*X.loc[:,feature2]
+                    X = pd.concat([X,newColumn], axis=1)
+                    X.columns.values[-1]= newFeature
+    return X
+X_train_poly = PolynomialFeatures(X_train, degree=MODEL_DEGREE)
+
+
+
+X_train_poly.head()
+
 
 # fit the transformed features to Linear Regression
 poly_model = linear_model.LinearRegression()
 poly_model.fit(X_train_poly, y_train)
-
 # predicting on training data-set
 y_train_predicted = poly_model.predict(X_train_poly)
-ypred=poly_model.predict(poly_features.transform(X_test))
+
+print("Model Training Error:" + str(mean_squared_error(y_train, y_train_predicted)))
+
+
+
+#Test data preprocessing
+X_test['Extracurricular Activities'] = le.fit_transform(X_test['Extracurricular Activities'])
+X_test = X_test[top_feature]
+
+X_test_poly = PolynomialFeatures(X_test, degree=MODEL_DEGREE)
 
 # predicting on test data-set
-prediction = poly_model.predict(poly_features.fit_transform(X_test))
+y_test_predicted = poly_model.predict(X_test_poly)
+
+print("Model Test Error:" + str(mean_squared_error(y_test, y_test_predicted)))
 
 
-print('Co-efficient of linear regression',poly_model.coef_)
-print('Intercept of linear regression model',poly_model.intercept_)
-print('Mean Square Error', metrics.mean_squared_error(y_test, prediction))
 
-true_player_value=np.asarray(y_test)[0]
-predicted_player_value=prediction[0]
-print('True value for the first player in the test set in millions is : ' + str(true_player_value))
-print('Predicted value for the first player in the test set in millions is : ' + str(predicted_player_value))
+
+
+
